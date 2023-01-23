c53503ba84c59477b0846b4131d480cd51738fd9
mozdalif <mozsocia@gmail.com>
2023-01-24 01:23:09 +0600

    Revert "Add files via upload"
    This reverts commit e1489da06cb6a7359a21f2273501d1acba827182.

--- ex_files/resources/views/users/show.blade.php
+++
@@ -1,3 +0,0 @@
-<p>Name: {{ $user->name }}</p>
-<p>Email: {{ $user->email }}</p>
-<a href="{{ route('users.index') }}">Back</a>
--- ex_files/storage/framework/views/ac0dada952ecbfe7f0cb7ff5c3acdfb1ed880e6b.php
+++
@@ -1,4 +0,0 @@
-<p>Name: <?php echo e($user->name); ?></p>
-<p>Email: <?php echo e($user->email); ?></p>
-<a href="<?php echo e(route('users.index')); ?>">Back</a>
-<?php /**PATH C:\laragon\www\laravel_8_basic\resources\views/users/show.blade.php ENDPATH**/ ?>
